045028b14a311fad252b4187f1558ac721f28fdd
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2023-01-14 17:29:04 -0300

    Class NewVisitorTest

--- chapter1/functional_tests.py
+++ chapter1/functional_tests.py
@@ -1,12 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Luciana has heard about a cool new online to-do app. She goes to check out its homepage
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
 
-# She notices the page title and header mention to-do lists
-assert "To-Do" in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Luciana has heard about a cool new online to-do app. She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test')
+        
+if __name__ == '__main__':
+    unittest.main(warnings = "ignore")
+
+
 
 # She is invited to enter a to-do item straight away
 
@@ -23,4 +38,3 @@ assert "To-Do" in browser.title
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
-browser.quit()
